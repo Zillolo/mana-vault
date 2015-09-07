c48e7eb705ccf0f0ea83f6ca9746cd0fa3db287e
Alex Egger <alex.egger96@gmail.com>
2015-09-07 23:02:08 +0200

    Add a function to add an entry to the budget module.

--- app/mod_budget/controller.py
+++ app/mod_budget/controller.py
@@ -1,7 +1,10 @@
-from flask import Blueprint, render_template, request
+from flask import Blueprint, flash, redirect, render_template, request, url_for
 
+from bson.objectid import ObjectId
+
+from app import logger
 from app.mod_budget.form import AddEntryForm
-from app.mod_budget.model import loadCategories
+from app.mod_budget.model import Category, Entry, loadCategories
 
 from app.mod_auth.helper import requireAuth
 
@@ -16,8 +19,19 @@ def default():
 @requireAuth()
 def addEntry():
     form = AddEntryForm(request.form)
+    # Load the categories from the db into the SelectField
     form.category.choices = loadCategories()
 
     if request.method == 'POST' and form.validate():
-        return "Lettn"
+        entry = Entry()
+        form.populate_obj(entry)
+
+        logger.debug(form.category.data)
+        # Insert category into reference field.
+        entry.category = Category.objects(id = ObjectId(entry.category)).first()
+
+        entry.save()
+        logger.info('A new entry has been saved.')
+        flash('Your entry has been added.')
+        return redirect(url_for('budget.default'))
     return render_template('budget/entry/add.html', form = form)
--- app/mod_budget/form.py
+++ app/mod_budget/form.py
@@ -1,7 +1,9 @@
 from wtforms import Form, DecimalField, SelectField, TextField, validators
 
+from bson.objectid import ObjectId
+
 class AddEntryForm(Form):
     amount = DecimalField('Amount', places = 2, validators = [validators.Required()])
     description = TextField('Description', validators = [validators.Required()])
 
-    category = SelectField()
+    category = SelectField(coerce = ObjectId)
