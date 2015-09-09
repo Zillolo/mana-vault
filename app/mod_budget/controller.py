a0d17ad660671b5cb0a6573ee0d646ae8409ff06
Alex Egger <alex.egger96@gmail.com>
2015-09-09 23:22:27 +0200

    Replace addExpense and addIncome with addEntry
    addEntry can do both functions job easily. The difference between
    expense and income can be specified by the asAsset parameter.

--- app/mod_budget/controller.py
+++ app/mod_budget/controller.py
@@ -16,32 +16,6 @@ budget = Blueprint('budget', __name__, template_folder = 'templates')
 def default():
     return "Hello World!"
 
-@budget.route('/income/add', methods = ['GET', 'POST'])
-@requireAuth()
-def addIncome():
-    form = AddEntryForm(request.form)
-    # Load the categories from the DB into the SelectField
-    form.loadCategories()
-
-    if request.method == 'POST' and form.validate():
-        income = Income()
-        form.populate_obj(income)
-
-        # Insert category into the ReferenceField.
-        income.category = Category.objects(id = ObjectId(income.category)).first()
-        # Insert owner into the ReferenceField.
-        userId = ObjectId(session.get('user')['_id']['$oid'])
-        income.owner = User.objects(id = userId).first()
-        income.save()
-
-        logger.debug('{0} added Income({1}, {2}, {3})'.format(
-            session.get('user')['username'], income.amount, income.description,
-                income.category.name))
-
-        flash('Your income has been added.')
-        return redirect(url_for('budget.default'))
-    return render_template('budget/income/add.html', form = form)
-
 @budget.route('/income/delete/<id>')
 @requireAuth()
 def deleteIncome(id):
@@ -59,32 +33,6 @@ def deleteIncome(id):
         flash('You are not authorized to delete this entry.')
     return redirect(url_for('budget.default'))
 
-@budget.route('/expense/add', methods = ['GET', 'POST'])
-@requireAuth()
-def addExpense():
-    form = AddEntryForm(request.form)
-    # Load the categories from the DB into the SelectField
-    form.loadCategories()
-
-    if request.method == 'POST' and form.validate():
-        expense = Expense()
-        form.populate_obj(expense)
-
-        # Insert category into the ReferenceField.
-        expense.category = Category.objects(id = ObjectId(expense.category)).first()
-        # Insert owner into the ReferenceField.
-        userId = ObjectId(session.get('user')['_id']['$oid'])
-        expense.owner = User.objects(id = userId).first()
-        expense.save()
-
-        logger.debug('{0} added Income({1}, {2}, {3})'.format(
-            session.get('user')['username'], expense.amount, expense.description,
-                expense.category.name))
-
-        flash('Your expense has been added.')
-        return redirect(url_for('budget.default'))
-    return render_template('budget/expense/add.html', form = form)
-
 @budget.route('/expense/delete/<id>')
 @requireAuth()
 def deleteExpense(id):
@@ -101,3 +49,33 @@ def deleteExpense(id):
     else:
         flash('You are not authorized to delete this entry.')
     return redirect(url_for('budget.default'))
+
+@budget.route('/entry/add', methods = ['GET', 'POST'])
+@requireAuth()
+def addEntry(asAsset = False):
+    form = AddEntryForm(request.form)
+    # Load the categories from the DB into the SelectField
+    form.loadCategories()
+
+    if request.method == 'POST' and form.validate():
+        entry = Entry()
+        form.populate_obj(entry)
+
+        # If this is an expense, multiply the amount by (-1).
+        if not asAsset:
+            entry.amount = entry.amount * (-1)
+
+        # Insert category into the ReferenceField.
+        entry.category = Category.objects(id = ObjectId(entry.category)).first()
+        # Insert owner into the ReferenceField.
+        userId = ObjectId(session.get('user')['_id']['$oid'])
+        entry.owner = User.objects(id = userId).first()
+        ientry.save()
+
+        logger.debug('{0} added Income({1}, {2}, {3})'.format(
+            session.get('user')['username'], entry.amount, entry.description,
+                entry.category.name))
+
+        flash('Your entry has been added.')
+        return redirect(url_for('budget.default'))
+    return render_template('budget/income/add.html', form = form)
