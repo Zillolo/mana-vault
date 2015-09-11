59ddc007eff3cdf293dccdc89cf72180fb6dd046
Alex Egger <alex.egger96@gmail.com>
2015-09-11 23:31:46 +0200

    Fix euro sign messing things up

--- app/mod_auth/controller.py
+++ app/mod_auth/controller.py
@@ -66,7 +66,7 @@ def login():
         if user is not None:
             if user.password == generateHash(form.password.data):
                 session['user'] = user
-                session['currency'] = '€'
+                session['currency'] = u'€'
                 return redirect(session.get('next', url_for('budget.showSummary')))
 
         logger.info('User %s has logged in.' % user.username)
